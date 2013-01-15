703813f7eb4a7e27f16af0673af0f11100bea84b
Jan Müller <jan.mueller.online@googlemail.com>
2013-01-15 23:31:39 +0100

    comments

--- NeuralNet.cpp
+++ NeuralNet.cpp
@@ -9,29 +9,29 @@ using namespace std;
 
 ANN_ERROR::file_exception::file_exception(const char* q){ p=q; }
 
-//Public Methods
+//Public Methoden
 
 void NeuralNet::setZero(){
 
 }
 void NeuralNet::mem_alloc(int input, int hidden, int output){
-	//save dimensions of the net
+	//Dimensionen des Netzes speichern
 	amountI=input; amountH=hidden; amountO=output;
-	//create weight matrices
+	//erstellen der Gewichtungsmatrix
 	ihWeight=new float*[amountI+1];
 	for(int i=0; i<=amountI; i++)
 		ihWeight[i]=new float[amountH];
 	hoWeight=new float*[amountH+1];
 	for(int i=0; i<=amountH; i++)
 		hoWeight[i]=new float[amountO];
-	//create weight difference matrices
+	//erstellen der Matrix für die Gewichtungsunterschiede
 	ihWeightDelta=new float*[amountI+1];
 	for(int i=0; i<=amountI; i++)
 		ihWeightDelta[i]=new float[amountH];
 	hoWeightDelta=new float*[amountH+1];
 	for(int i=0; i<=amountH; i++)
 		hoWeightDelta[i]=new float[amountO];
-	//allocate memory for other matrices
+	//Speicher allozieren für die restlichen Arrays
 	preferenceO=new float[amountO];
 	inputI=new float[amountI];
 	inputH=new float[amountH];
@@ -42,33 +42,33 @@ void NeuralNet::mem_alloc(int input, int hidden, int output){
 	errorH=new float[amountH+1];
 	errorO=new float[amountO];
 	differenceO=new float[amountO];
-	//bring differenceO in valid state for getE()
+	//differenceO bereitmachen für die getE()
 	for(int i=0; i<amountO; i++)
 		differenceO[i]=0.0;
-	//set delta matrices to 0 (start value)
+	//Gewichtungsunterschiedmatrix auf Startwert setzen
 	for(int i=0; i<=amountI; i++)
 		for(int j=0; j<amountH; j++)
 				ihWeightDelta[i][j]=0.0;
 	for(int i=0; i<=amountH; i++)
 		for(int j=0; j<amountO; j++)
 				hoWeightDelta[i][j]=0.0;
-	//setting default learning rate
+	//Default Lern Rate setzen
 	learnRate=2.0;
-	//setting default error tolerance
+	//Default Fehler Toleranz setzen
 	errorTolerance=0.1;
-	//setting default impuls rate
+	//Default Impuls Rate setzen
 	impuls=0.4;
 }
 void NeuralNet::backprop(){
 
 }
 
-//Private Methods
+//Private Methoden
 
 NeuralNet::NeuralNet(int input, int hidden, int output){
 	mem_alloc(input,hidden,output);
 	srand(time(NULL));
-	//fill with rand values (low end...high end)
+	//Füllen der Gewichtungsmatrix mit Werten zwischen -0,3 und 0,3
 	for(int i=0; i<=amountI; i++)
 		for(int j=0; j<amountH; j++)
 			ihWeight[i][j]=(float)rand()/RAND_MAX*0.6-0.3;
@@ -78,7 +78,7 @@ NeuralNet::NeuralNet(int input, int hidden, int output){
 }
 NeuralNet::NeuralNet(int input, int hidden, int output, float **inpHidWeight, float **hidOutWeight){
 	NeuralNet(input, hidden, output);
-	//create weight matrices with user-defined values
+	//Befüllen der Gewichtungsmatrix mit Übergabe Werten
 	for(int i=0; i<=amountI; i++)
 		for(int j=0; j<amountH; j++)
 			ihWeight[i][j]=inpHidWeight[i][j];
@@ -93,7 +93,7 @@ NeuralNet::NeuralNet(char *file){
 		throw ANN_ERROR::file_exception("Error opening file");
 		return;
 	}
-	//throw exception on input failing
+	//Exception Ausgabe wenn Datei einlesen fehlschlägt
 	in.exceptions(ios_base::failbit|ios_base::badbit|ios_base::eofbit);
 	try{
 		in>>input;
